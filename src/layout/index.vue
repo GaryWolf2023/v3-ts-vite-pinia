daed6a89778e0a34e8c01d2c79ed5558d7de6aa7
husky-lby <husky._lby@petalmail.com>
2023-02-27 18:32:13 +0800

    first

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const a = ref(10)
+onMounted(() => {
+  console.log(a)
+})
+</script>
+<template>
+  <div>
+    <aside>1</aside>
+    <div>
+      <header>1</header>
+      <second>2</second>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+   //配置sass
+   css: {
+    preprocessorOptions: {
+      scss: {
+        // '@import "assets/scss/globalVar.scss";@import "assets/scss/globalMixin.scss";'
+        additionalData: '@import "./styles/scss/globalVar.scss";@import "./styles/scss/globalMixin.scss";'
+      }
+    }
+  },
+})
